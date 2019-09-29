eca7257fa27db606f964f35b66b122bbdbe5de2b
Shakeel Nijabdeen <shakeelnijabden@gmail.com>
2019-09-29 15:15:21 +0530

    Test Cases

---
+++ procurementapp/test/appTest.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+const app = require('../src/app');
+
+//Check authentication
+//Companies - get request
+describe('App', function(){
+    it('Check if get request for Companies is successful', function(){
+        
+    })
+})
