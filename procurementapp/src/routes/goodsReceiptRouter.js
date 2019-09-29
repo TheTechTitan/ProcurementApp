cac3bd4b9751bef0b3f1f56dc4a6699134a172a9
Binali19 <42922663+Binali19@users.noreply.github.com>
2019-09-29 06:47:52 +0530

    Routers added
    goodsReceiptRouter, invoiceRouter, SupplyChainManagerRouter, deliveryRouter added

---
+++ procurementapp/src/routes/goodsReceiptRouter.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+var goodsReceiptController = require('../controller/goodsReceiptController');
+
+router.post('/', (req, res) => {
+    goodsReceiptController.insert(req.body).then(data => {
+        res.status(data.status).send({message: data.message});
+    }).catch(err => {
+        res.status(err.status).send({message: err.message});
+    })
+});
+
+router.put('/:id', (req, res) => {
+    goodsReceiptController.update(req.params.id, req.body).then(data => {
+        res.status(data.status).send({message: data.message, data: data.data});
+    }).catch(err => {
+        res.status(err.status).send({message: err.message});
+    })
+});
+
+router.get('/', (req, res) => {
+    goodsReceiptController.getAll().then(data => {
+        res.status(data.status).send({data: data.data});
+    }).catch(err => {
+        res.status(err.status).send({message: err.message});
+    });
+});
+
+router.get('/:id', (req, res) => {
+    goodsReceiptController.getOne(req.params.id).then(data => {
+        res.status(data.status).send({data: data.data});
+    }).catch(err => {
+        res.status(err.status).send({message: err.message});
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    goodsReceiptController.delete(req.params.id).then(data => {
+        res.status(data.status).send({message: data.message});
+    }).catch(err => {
+        res.status(err.status).send({message: err.message});
+    })
+});
+
+module.exports = router;
